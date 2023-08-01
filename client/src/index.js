6f78060f2dc904db6d01ae05a1b6c0ddb8e6288c
D00m <ivangromov1993@gmail.com>
2023-08-01 23:12:06 +0200

    Small fix to remove unnecessary layer from the entry point. App component wasn't doing anything

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,28 @@
-import './index.css';
+import './index.scss';
+
+import 'fontsource-roboto';
+import '@fontsource/montserrat/100.css';
+import '@fontsource/montserrat/200.css';
+import '@fontsource/montserrat/300.css';
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/500.css';
+import '@fontsource/montserrat/600.css';
+import '@fontsource/montserrat/700.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from './App';
 import store from './store/store';
 
-import { Provider } from 'react-redux';
+import MainLayout from './views/mainLayout';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <App/>
+    <BrowserRouter>
+      <MainLayout/>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
